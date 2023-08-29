88627249e6ac3ad4b0e38bb76fb886cc321720ef
tomvargas <tomvargasd@gmail.com>
2023-08-29 10:50:28 -0500

    test4

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,22 +1,6 @@
 let slideIndex = 0;
 $(function () {
   showSlides();
-  //set example net images for all imgId img1, img2, img3, img4
-  $("img[imgId='img1']").attr("src", "https://via.placeholder.com/150");
-  $("img[imgId='img2']").attr("src", "https://via.placeholder.com/150");
-  $("img[imgId='img3']").attr("src", "https://via.placeholder.com/150");
-  $("img[imgId='img4']").attr("src", "https://via.placeholder.com/150");
-
-  //set example net images for all imgId prod1, prod2, prod3, prod4
-  $("img[imgId='prod1']").attr("src", "https://via.placeholder.com/150");
-  $("img[imgId='prod2']").attr("src", "https://via.placeholder.com/150");
-  $("img[imgId='prod3']").attr("src", "https://via.placeholder.com/150");
-  $("img[imgId='prod4']").attr("src", "https://via.placeholder.com/150");
-
-  //set example net images for all imgId related1, related2, related3
-  $("img[imgId='related1']").attr("src", "https://via.placeholder.com/150");
-  $("img[imgId='related2']").attr("src", "https://via.placeholder.com/150");
-  $("img[imgId='related3']").attr("src", "https://via.placeholder.com/150");
 });
 function showSlides() {
   let slides = document.getElementsByClassName("mySlides");
